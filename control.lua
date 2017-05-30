d698f6ebcaa5c33ee36c0b0141941e03e4262129
Dustine Camacho <dustine.camacho@gmail.com>
2017-05-30 03:10:52 +0100

    Reverted bot smoothing, started rotating support

--- control.lua
+++ control.lua
@@ -5,7 +5,7 @@ local Position = require "stdlib/area/position"
 local Entity = require "stdlib/entity/entity"
 require "stdlib/event/event"
 
-local nodes, unpaired, indexes, bot_names, charging
+local nodes, unpaired, indexes, bot_names
 -- local bot_blacklist = {}
 -- bot_blacklist["logistic-robot"] = false
 -- bot_blacklist["construction-robot"] = false
@@ -16,15 +16,13 @@ local function globalToLocal(isInit)
     global.ChargeTransmission.charger_nodes = global.ChargeTransmission.charger_nodes or {}
     global.ChargeTransmission.unpaired_chargers = global.ChargeTransmission.unpaired_chargers or {}
     global.ChargeTransmission.node_indexes = global.ChargeTransmission.node_indexes or {}
-    global.ChargeTransmission.bot_names = global.ChargeTransmission.bot_names or {construction = {}, logistic={}}
-    global.ChargeTransmission.bots_charging = global.ChargeTransmission.bots_charging or {}
+    global.ChargeTransmission.bot_names = global.ChargeTransmission.bot_names or {}
   end
 
   nodes = global.ChargeTransmission.charger_nodes
   unpaired = global.ChargeTransmission.unpaired_chargers
   indexes = global.ChargeTransmission.node_indexes
   bot_names = global.ChargeTransmission.bot_names
-  charging = global.ChargeTransmission.bots_charging
 end
 
 -- PLACEHOLDER for settings and chargeless bots detection
@@ -33,18 +31,16 @@ local function isValidBotProto(proto)
 end
 
 local function registerBotNames()
-  charging = {}
-  bot_names.logistic = {}
-  bot_names.construction = {}
+  bot_names = {}
 
   for _, proto in pairs(game.entity_prototypes) do
     if proto.type == "logistic-robot" then
       if isValidBotProto(proto) then
-        bot_names.logistic[proto.name] = proto.max_energy
+        bot_names[proto.name] = proto.max_energy
       end
     elseif proto.type == "construction-robot" then
       if isValidBotProto(proto) then
-        bot_names.construction[proto.name] = proto.max_energy
+        bot_names[proto.name] = proto.max_energy
       end
     end
   end
@@ -72,7 +68,6 @@ local function findNeighbourCells(charger)
   local distance = math.huge
   for i=1,#neighbours do
     local new_distance = Position.distance_squared(charger.position, neighbours[i].owner.position)
-    log(i..":distance:"..new_distance)
     if new_distance < distance then distance = new_distance; index = i end
   end
   if index then
@@ -92,7 +87,6 @@ local function registerCharger(charger, cell)
     -- register the node
     table.insert(nodes, node)
     indexes[node.unit_number] = #nodes
-    log("["..node.unit_number.."]:new")
     return node
   end
   local node = nodes[index]
@@ -108,10 +102,13 @@ end
 --   Else, adds it as an unpaired charger
 local function onBuildCharger(entity)
   if(entity.name:find("charge%-transmission%-charger")) then
+    entity.destructible = false
     local powerbox = entity.surface.create_entity{
       name = "charge-transmission-charger-powerbox",
-      position = entity.position
+      position = entity.position,
+      force = entity.force
     }
+    Entity.set_data(powerbox, {main = entity})
     local data = {powerbox = powerbox}
     data.index, data.cell = findNeighbourCells(entity)
     Entity.set_data(entity, data)
@@ -119,7 +116,6 @@ local function onBuildCharger(entity)
       registerCharger(entity, data.cell)
     else
       table.insert(unpaired, entity)
-      log("unpair+")
     end
   end
 end
@@ -134,13 +130,13 @@ local function onMinedCharger(charger)
 
     -- remove oneself from nodes
     for _, node in pairs(nodes) do
-      local chargers = Entity.get_data(node)
-      for key, c in pairs(chargers) do
+      local node_data = Entity.get_data(node)
+      for key, c in pairs(node_data.chargers) do
         if c.unit_number == charger.unit_number then
-          table.remove(chargers, key)
-          -- return
+          table.remove(node_data.chargers, key)
         end
       end
+      Entity.set_data(node, node_data)
     end
   end
 end
@@ -151,10 +147,48 @@ Event.register(defines.events.on_robot_built_entity, function(event) onBuildChar
 Event.register(defines.events.on_player_mined_entity, function(event) onMinedCharger(event.entity) end)
 Event.register(defines.events.on_robot_mined_entity, function(event) onMinedCharger(event.entity) end)
 
--- TODO: Swap roboports when you "rotate" the bot charger
+-- TODO: Refactor and test this code, as it's basically snippets from all around!
 Event.register(defines.events.on_player_rotated_entity, function(event)
-  if(event.entity.name:find("charge%-transmission%-charger")) then
+  if(event.entity.name:find("charge%-transmission%-charger%-powerbox")) then
     log("yes")
+    local data = Entity.get_data(event.entity)
+    local charger = data.main
+    local data_charger = Entity.get_data(charger)
+    -- unregister from old cell
+    log("unregister")
+    local nid = indexes[data_charger.cell.owner.unit_number]
+    local node = nodes[nid]
+    local data_node = Entity.get_data(node)
+    for key, c in pairs(data_node.chargers) do
+      if c.unit_number == charger.unit_number then
+        table.remove(data_node.chargers, key)
+      end
+    end
+    Entity.set_data(node, data_node)
+    log("register")
+    -- enter the new cell
+    local neighbours = charger.logistic_cell.neighbours
+    data_charger.index = (data_charger.index+1)%(#neighbours)
+    log(serpent.block(neighbours))
+    data_charger.cell = neighbours[data_charger.index]
+    Entity.set_data(charger, data_charger)
+    local index = indexes[data_charger.cell.owner.unit_number]
+    if not index then
+      log("new")
+      -- new node
+      node = data_charger.cell.owner
+      data_node = {chargers = {charger}}
+      data_node.area = Position.expand_to_area(node.position, node.logistic_cell.construction_radius)
+      Entity.set_data(node, data_node)
+      -- register the node
+      table.insert(nodes, node)
+      indexes[node.unit_number] = #nodes
+      return node
+    end
+    node = nodes[index]
+    data_node = Entity.get_data(node)
+    table.insert(data_node.chargers, charger)
+    Entity.set_data(node, data_node)
   end
 end)
 
@@ -166,6 +200,9 @@ Event.register(defines.events.on_selected_entity_changed, function(event)
   local current_entity = player.selected
   if current_entity and current_entity.name:match("charge%-transmission%-charger") then
     local data = Entity.get_data(current_entity)
+    if current_entity.name ~= "charge-transmission-charger" then
+      data = Entity.get_data(data.main)
+    end
     if data.cell then
       player.set_gui_arrow{type="entity", entity=data.cell.owner}
     end
@@ -183,6 +220,22 @@ end)
 -- * code check
 -- * add more .valid checks
 script.on_event(defines.events.on_tick, function(event)
+  -- charger re-pairing
+  for cid=1+event.tick%150,#unpaired,150 do
+    local charger = unpaired[cid]
+    if charger and charger.valid then
+      local data = Entity.get_data(charger)
+      data.index, data.cell = findNeighbourCells(charger)
+      Entity.set_data(charger, data)
+      if data.cell then
+        registerCharger(charger, data.cell)
+        table.remove(unpaired, cid)
+      end
+    else
+      table.remove(unpaired, cid)
+    end
+  end
+
   -- clear invalid nodes before iterating nodes
   if event.tick%30 == 0 then
     for nid=#nodes,1,-1 do
@@ -195,126 +248,73 @@ script.on_event(defines.events.on_tick, function(event)
           local charger_data = Entity.get_data(charger)
           charger_data.powerbox.power_usage = 0
           charger_data.index = nil
-          data.cell = nil
+          charger_data.cell = nil
           Entity.set_data(charger, charger_data)
           table.insert(unpaired, charger)
-          log("["..node.unit_number.."]:delete:"..#(data.chargers))
         end
-        table.remove(nodes, n)
+        table.remove(nodes, nid)
       end
-    end
-  end
-
-  -- charger re-pairing
-  for cid=1+event.tick%150,#unpaired,150 do
-    local charger = unpaired[cid]
-    if charger and charger.valid then
-      local data = Entity.get_data(charger)
-      data.index, data.cell = findNeighbourCells(charger)
-      Entity.set_data(charger, data)
-      if data.cell then
-        registerCharger(charger, data.cell)
-        table.remove(unpaired, cid)
-        log("register:"..charger.unit_number)
-      end
-    else
-      table.remove(unpaired, cid)
     end
   end
 
   -- area scanning
   for nid=1+event.tick%30,#nodes,30 do
     local node = nodes[nid]
-    local data = Entity.get_data(node)
-    if node.valid and #(data.chargers) > 0 then
+    local node_data = Entity.get_data(node)
+    if node.valid and #(node_data.chargers) > 0 then
       local target_bots = {}
       local energy = 0
       local cost = 0
-      local bot_count = 0
 
       -- check total energy cost
       local constrobots, logibots =
         node.surface.find_entities_filtered {
-          area = data.area,
+          area = node_data.area,
           force = node.force,
           type = "construction-robot"
         },
         node.surface.find_entities_filtered {
-          area = data.area,
+          area = node_data.area,
           force = node.force,
           type = "logistic-robot"
         }
       for bid=1,#constrobots + #logibots do
-        local bot, max_energy
-        if bid <= #constrobots then
-          bot = constrobots[bid]
-          max_energy = bot_names.construction[bot.name]
-        else
-          bot = logibots[bid - #constrobots]
-          max_energy = bot_names.logistic[bot.name]
-        end
+        local bot
+        if bid <= #constrobots then bot = constrobots[bid]
+        else bot = logibots[bid - #constrobots] end
+        local max_energy = bot_names[bot.name]
 
         if bot and max_energy then
-          cost = cost + (max_energy - bot.energy) * 2
-          bot_count = bot_count + 1
-
-          local tid = (math.floor(bot_count/50) + (event.tick + 1))%30
-          target_bots[tid] = target_bots[tid] or {}
-          table.insert(target_bots[tid], bot)
+          cost = cost + (max_energy - bot.energy)
+          table.insert(target_bots, {bot = bot, max = max_energy})
         end
       end
 
       -- calculate total available energy
-      for cid=#(data.chargers),1,-1 do
-        if data.chargers[cid].valid then
-          local powerbox = Entity.get_data(data.chargers[cid]).powerbox
+      for cid=#(node_data.chargers),1,-1 do
+        if node_data.chargers[cid].valid then
+          local powerbox = Entity.get_data(node_data.chargers[cid]).powerbox
           energy = energy + powerbox.energy
         else
-          table.remove(data.chargers, cid)
+          table.remove(node_data.chargers, cid)
         end
       end
-
-      -- log("cost:"..bot_count..":"..cost)
-      -- log("energy:"..#(data.chargers)..":"..energy)
 
       local fraction = 1
       -- overspending so the machine charges less per robot
       if cost > energy then fraction = energy / cost end
 
       -- set power cost on the powerboxes
-      for cid=1, #(data.chargers) do
-        local powerbox = Entity.get_data(data.chargers[cid]).powerbox
-        powerbox.power_usage = (cost * fraction) / (30 * #(data.chargers))
+      for cid=1, #(node_data.chargers) do
+        local powerbox = Entity.get_data(node_data.chargers[cid]).powerbox
+        powerbox.power_usage = (cost * fraction) / (30 * #(node_data.chargers))
       end
 
-      -- insert the to-be-charged bots on charging
-      for bid=1,30 do
-        if target_bots[bid] then
-          charging[bid] = charging[bid] or {}
-          table.insert(charging[bid], {      
-            bots = target_bots[bid],
-            area = data.area,
-            fraction = fraction
-          })
-          -- log("["..node.unit_number.."]:+"..#(target_bots[bid]).." bots:"..event.tick..":f"..fraction)
-        end
+      -- charge ALL bots
+      for tid=1, #target_bots do
+        local target = target_bots[tid]
+        target.bot.energy = target.bot.energy + (target.max - target.bot.energy) * fraction
       end
     end
   end
-
-  -- bot recharging
-  -- if charging[event.tick%30] then
-  --   local charging_set = charging[event.tick%30]
-  --   for i=1,#charging_set do
-  --     local list = charging_set[i]
-  --     for j=1, #(list.bots) do
-  --       local bot = list.bots[j]
-  --       if bot and bot.valid then
-  --         bot.energy = bot.energy + (bot.prototype.max_energy - bot.energy) * list.fraction
-  --       end
-  --     end
-  --   end
-
-  --   charging[event.tick%30] = nil
-  -- end
 end)
